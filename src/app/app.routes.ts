714c9b825c7888abe0a63098d7f805504aa0b284
Victor Terrones Huaman <trex4vi@gmail.com>
2025-09-07 14:38:14 -0500

    (Victor)08/09/2025

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = 
+[
+    {
+        path: 'login',component: Login, 
+    },
+
+    {
+        path: 'home', component: Home,
+    },
+
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, // Redirección inicial
+
+];
